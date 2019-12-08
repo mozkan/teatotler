8383d6c3c6635ed7ef729e9f347804269e32eac2
Murat Ozkan <murat.ozkan946@gmail.com>
2019-12-07 20:01:47 -0800

    Updated include paths
    Updated include paths to include the new linked directories.

--- sw/app/main.cpp
+++ sw/app/main.cpp
@@ -1,10 +1,20 @@
 #include <cr_section_macros.h>
 
-int main(void) {
-    volatile static int i = 0 ;
-    while(1) {
-        i++ ;
-    }
+#include "mcu/gpio.h"
+#include "mcu/pinmux.h"
+#include "mcu/pins.h"
 
-    return 0 ;
+int main(void) {
+  mcu::PinMux mux;
+
+  mcu::Gpio gpio_btn_debug(mcu::McuPin::kPIO0_12, mux);
+  gpio_btn_debug.SetDirectionToInput();
+  gpio_btn_debug.EnablePullup();
+
+  volatile static int i = 0 ;
+  while(1) {
+      i++ ;
+  }
+
+  return 0 ;
 }
