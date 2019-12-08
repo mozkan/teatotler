d0db6d2939aa2e9b45189164a9ceb2423649d6c7
Murat Ozkan <murat.ozkan946@gmail.com>
2019-12-08 00:25:35 -0800

    Confirmed LEDs work
    Test code initializes LED array as I/O (instead of PWM). Works.

--- sw/app/main.cpp
+++ sw/app/main.cpp
@@ -27,16 +27,45 @@ int main(void) {
   gpio_btn_lower.SetDirectionToInput();
   gpio_btn_lower.SetResistorMode(mcu::Gpio::ResistorMode::kPullUp);
 
+  mcu::Gpio gpio_led_row1(mcu::McuPio::kPIO0_19);
+  gpio_led_row1.Set();
+  gpio_led_row1.SetDirectionToOutput();
+
+  mcu::Gpio gpio_led_row2(mcu::McuPio::kPIO0_22);
+  gpio_led_row2.Set();
+  gpio_led_row2.SetDirectionToOutput();
+
+  mcu::Gpio gpio_led_row3(mcu::McuPio::kPIO0_18);
+  gpio_led_row3.Set();
+  gpio_led_row3.SetDirectionToOutput();
+
+  mcu::Gpio gpio_led_col1(mcu::McuPio::kPIO0_17);
+  gpio_led_col1.Set();
+  gpio_led_col1.SetDirectionToOutput();
+
+  mcu::Gpio gpio_led_col2(mcu::McuPio::kPIO0_21);
+  gpio_led_col2.Set();
+  gpio_led_col2.SetDirectionToOutput();
+
+  mcu::Gpio gpio_led_col3(mcu::McuPio::kPIO0_20);
+  gpio_led_col3.Set();
+  gpio_led_col3.SetDirectionToOutput();
+
   volatile int i = 0;
 
   while(1) {
-    while (gpio_btn_lower.Read() != mcu::Gpio::Value::kLow) {
-      i++;
-    }
-
-    while (gpio_btn_lower.Read() != mcu::Gpio::Value::kHigh) {
-      i++;
-    }
+    gpio_led_col1.Clear();
+    while (gpio_btn_lower.Read() != mcu::Gpio::Value::kLow) { i++; }
+    gpio_led_col1.Set();
+    while (gpio_btn_lower.Read() != mcu::Gpio::Value::kHigh) { i++; }
+    gpio_led_col2.Clear();
+    while (gpio_btn_lower.Read() != mcu::Gpio::Value::kLow) { i++; }
+    gpio_led_col2.Set();
+    while (gpio_btn_lower.Read() != mcu::Gpio::Value::kHigh) { i++; }
+    gpio_led_col3.Clear();
+    while (gpio_btn_lower.Read() != mcu::Gpio::Value::kLow) { i++; }
+    gpio_led_col3.Set();
+    while (gpio_btn_lower.Read() != mcu::Gpio::Value::kHigh) { i++; }
   }
 
   return 0;
