af9fb1f6226f5ef95596f96fba9be0883a348b7e
Murat Ozkan <murat.ozkan946@gmail.com>
2020-01-26 14:29:44 -0800

    Updated I/O mappings for Rev B board
    Just did the first test of the Rev B board. Old bringup code still
    works. Motor is weak. Not sure why. Probably not software.

--- sw/app/main.cpp
+++ sw/app/main.cpp
@@ -9,10 +9,10 @@
 class BasicHBridge {
  public:
   BasicHBridge()
-      : gpio_h_bridge_dir_a_(mcu::McuPio::kPIO0_0),
-        gpio_h_bridge_dir_b_(mcu::McuPio::kPIO0_14),
-        gpio_h_bridge_pwm_a_(mcu::McuPio::kPIO0_16),
-        gpio_h_bridge_pwm_b_(mcu::McuPio::kPIO0_15) {
+      : gpio_h_bridge_dir_a_(mcu::McuPio::kPIO0_9),
+        gpio_h_bridge_dir_b_(mcu::McuPio::kPIO0_8),
+        gpio_h_bridge_pwm_a_(mcu::McuPio::kPIO0_19),
+        gpio_h_bridge_pwm_b_(mcu::McuPio::kPIO0_20) {
     Stop();
 
     gpio_h_bridge_dir_a_.SetDirectionToOutput();
@@ -56,7 +56,7 @@ class BasicHBridge {
 int main(void) {
   mcu::Mcu mcu;
 
-  mcu::Gpio gpio_btn_debug(mcu::McuPio::kPIO0_12);
+  mcu::Gpio gpio_btn_debug(mcu::McuPio::kPIO0_10);
   gpio_btn_debug.SetDirectionToInput();
   gpio_btn_debug.SetResistorMode(mcu::Gpio::ResistorMode::kPullUp);
 
@@ -64,39 +64,39 @@ int main(void) {
   gpio_btn_start.SetDirectionToInput();
   gpio_btn_start.SetResistorMode(mcu::Gpio::ResistorMode::kPullUp);
 
-  mcu::Gpio gpio_btn_reset(mcu::McuPio::kPIO0_28);
+  mcu::Gpio gpio_btn_reset(mcu::McuPio::kPIO0_11);
   gpio_btn_reset.SetDirectionToInput();
   gpio_btn_reset.SetResistorMode(mcu::Gpio::ResistorMode::kPullUp);
 
-  mcu::Gpio gpio_btn_raise(mcu::McuPio::kPIO0_24);
+  mcu::Gpio gpio_btn_raise(mcu::McuPio::kPIO0_6);
   gpio_btn_raise.SetDirectionToInput();
   gpio_btn_raise.SetResistorMode(mcu::Gpio::ResistorMode::kPullUp);
 
-  mcu::Gpio gpio_btn_lower(mcu::McuPio::kPIO0_25);
+  mcu::Gpio gpio_btn_lower(mcu::McuPio::kPIO0_23);
   gpio_btn_lower.SetDirectionToInput();
   gpio_btn_lower.SetResistorMode(mcu::Gpio::ResistorMode::kPullUp);
 
-  mcu::Gpio gpio_led_row1(mcu::McuPio::kPIO0_19);
+  mcu::Gpio gpio_led_row1(mcu::McuPio::kPIO0_25);
   gpio_led_row1.Set();
   gpio_led_row1.SetDirectionToOutput();
 
-  mcu::Gpio gpio_led_row2(mcu::McuPio::kPIO0_22);
+  mcu::Gpio gpio_led_row2(mcu::McuPio::kPIO0_1);
   gpio_led_row2.Set();
   gpio_led_row2.SetDirectionToOutput();
 
-  mcu::Gpio gpio_led_row3(mcu::McuPio::kPIO0_18);
+  mcu::Gpio gpio_led_row3(mcu::McuPio::kPIO0_26);
   gpio_led_row3.Set();
   gpio_led_row3.SetDirectionToOutput();
 
-  mcu::Gpio gpio_led_col1(mcu::McuPio::kPIO0_17);
+  mcu::Gpio gpio_led_col1(mcu::McuPio::kPIO0_27);
   gpio_led_col1.Set();
   gpio_led_col1.SetDirectionToOutput();
 
-  mcu::Gpio gpio_led_col2(mcu::McuPio::kPIO0_21);
+  mcu::Gpio gpio_led_col2(mcu::McuPio::kPIO0_15);
   gpio_led_col2.Set();
   gpio_led_col2.SetDirectionToOutput();
 
-  mcu::Gpio gpio_led_col3(mcu::McuPio::kPIO0_20);
+  mcu::Gpio gpio_led_col3(mcu::McuPio::kPIO0_24);
   gpio_led_col3.Set();
   gpio_led_col3.SetDirectionToOutput();
 
